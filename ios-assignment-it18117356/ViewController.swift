40fc3eb0e11f2db994d8b25a00d25050abb1f2a9
Chiran Nuwan Bandara <37494456+nuwanchiran@users.noreply.github.com>
2021-05-05 21:59:53 +0530

    Initial Commit

---
+++ ios-assignment-it18117356/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-assignment-it18117356
+//
+//  Created by Chiran Nuwan on 2021-05-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
